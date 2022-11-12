4261816073fab914d0d3e09905adca5c076a144b
Phan Hao <nphaohcmus@gmail.com>
2022-11-12 11:40:09 +0700

    first commit

---
+++ app/src/main/java/edu/poly/test1/MainActivity.java
@@ -0,0 +1,24 @@
+package edu.poly.test1;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
